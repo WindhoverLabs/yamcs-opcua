cb5c28fb649a056a745cf744f5835f06d05fadc5
lgomez <lgomez@windhoverlabs.com>
2024-06-04 16:52:51 -0500

    -OPCUA server for unit testing

--- src/test/java/com/windhoverlabs/yamcs/opcua/test/OPCUALinkTest.java
+++ src/test/java/com/windhoverlabs/yamcs/opcua/test/OPCUALinkTest.java
@@ -2,12 +2,19 @@ package com.windhoverlabs.yamcs.opcua.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 /** Unit test for simple App. */
 public class OPCUALinkTest extends AbstractIntegrationTest {
 
   String yamcsInstance2 = "IntegrationTest";
+
+  @BeforeAll
+  public static void initOPCUAServer() throws Exception {
+    //    setupYamcs();
+    //	  FIXME:Add dummy server to run tests
+  }
 
   @Test
   public void testOPCUALink() {
