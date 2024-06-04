cdab803e8f7bc73565ab98845fa6126c31df6e6a
lgomez <lgomez@windhoverlabs.com>
2024-06-04 15:49:55 -0500

    -Add unit tests. WIP.

---
+++ src/test/java/com/windhoverlabs/yamcs/opcua/test/OPCUALinkTest.java
@@ -0,0 +1,16 @@
+package com.windhoverlabs.yamcs.opcua.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/** Unit test for simple App. */
+public class OPCUALinkTest extends AbstractIntegrationTest {
+
+  String yamcsInstance2 = "IntegrationTest";
+
+  @Test
+  public void testOPCUALink() {
+    assertEquals(true, true);
+  }
+}
