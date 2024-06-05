b940d983abd04fc36476593aa72ae4790101bae7
lgomez <lgomez@windhoverlabs.com>
2024-06-05 10:39:26 -0500

    -Minimal junit config

--- src/test/java/com/windhoverlabs/yamcs/opcua/test/OPCUALinkTest.java
+++ src/test/java/com/windhoverlabs/yamcs/opcua/test/OPCUALinkTest.java
@@ -2,22 +2,39 @@ package com.windhoverlabs.yamcs.opcua.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.yamcs.client.ClientException;
 
 /** Unit test for simple App. */
-public class OPCUALinkTest extends AbstractIntegrationTest {
+public class OPCUALinkTest {
 
   String yamcsInstance2 = "IntegrationTest";
+  String test = "";
 
-  @BeforeAll
-  public static void initOPCUAServer() throws Exception {
-    //    setupYamcs();
+  @BeforeEach
+  public void before() throws ClientException {
+    System.out.println("setupYamcs*************call");
+    try {
+      //      setupYamcs();
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
     //	  FIXME:Add dummy server to run tests
+    //    test = "testVar";
   }
 
   @Test
   public void testOPCUALink() {
-    assertEquals(true, true);
+    System.out.println("setupYamcs*************call3");
+    assertEquals(test, "");
+  }
+
+  @Test
+  public void testOPCUALink2() {
+    System.out.println("setupYamcs*************call4");
+    assertEquals(test, "");
   }
 }
